f5f46bd2915cf343bdc330cc1a64dcee854a116c
Madhuri-Dilliker <madhurid@getreadyguilford.org>
2021-01-25 14:46:45 -0500

    Search Component

---
+++ src/components/ui/Search.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react'
+
+
+const Search = () => {
+    return (
+        <section className='search'>
+            <form>
+                <input type='text' 
+                className='form-control' 
+                placeholder='Search characters'
+                autofocus
+                />
+            </form>
+            
+        </section>
+    )
+}
+
+export default Search
